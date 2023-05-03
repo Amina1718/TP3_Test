b35cd30162064d416e5d10bcc9c6de59b7234e48
Amina1718 <amina.bennajib@gmail.com>
2023-05-03 21:56:59 +0200

    ITER_7

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -1,47 +1,85 @@
 package ilu2;
 
+
 public class Welcome {
 	public static String welcome(String input) {
 	    if (input == null || input.trim().isEmpty()) {
 	        return "Hello, my friend";
 	    }
 
-	    StringBuilder phrase = new StringBuilder("Hello,");
-	    StringBuilder majuscule = new StringBuilder();
-	    String[] noms = input.split(","); // Séparer les noms en utilisant la virgule comme séparateur
-	    int length = noms.length;
+	    StringBuilder phrase = new StringBuilder("Hello, ");
+	    String[] noms = input.split(",");
+	    StringBuilder majuscule = new StringBuilder(phrase.toString().toUpperCase());
 
-	    for (int i = 0; i < length; i++) {
-	        String nom = noms[i].trim();
-	        if (nom.toUpperCase().equals(nom)) { // Vérifier si le nom est en majuscule
-	            if (majuscule.length() > 0) { //ajouter une virgule si nécessaire et ajouter le nom en majuscule au StringBuilder
-	                majuscule.append(",");
-	            }
-	            majuscule.append(" " + nom);
+	    if (GestionPrenoms(noms, phrase, majuscule)) {
+			return GestionCrie(phrase,majuscule,input).toString(); //ici , au moins un prenom en majuscule
+		}
+		
+		return phrase.toString();
+	}
+	
+	private static void ajouterPrenomAvecMajuscule(StringBuilder message, String nom) {
+	    if (nom == null || nom.isEmpty()) {
+	        return;
+	    }
+	    message.append(nom.substring(0, 1).toUpperCase()).append(nom.substring(1)).append(", ");
+	}
+
+	// Gère la virgule à la fin de la phrase, en enlevant la virgule si elle n'est pas nécessaire
+	private static void GestionVirgule(StringBuilder string) {
+		if(string.lastIndexOf(",") == string.length()-2) {
+			string.delete((string.length())-2, string.length());
+		}
+		else {
+			string.delete((string.length())-1, string.length());
+		}
+	}
+	
+	
+	private static StringBuilder GestionCrie(StringBuilder phrase,StringBuilder maj, String input) {
+		maj.append(" !");
+		if (input.contains(",")) {
+			phrase.append(". AND ");
+			return phrase.append(maj.toString().toUpperCase());
+		}
+		return maj;
+	}
+	
+	
+	// Ajoute "and" avant le dernier nom s'il y a plus de deux noms
+	private static void ajoutAnd(StringBuilder phrase) {
+		String[] separator = phrase.toString().split("(?<=,)");
+		
+		if(separator.length > 2) {
+			StringBuilder penultiemeTerme = new StringBuilder(separator[separator.length-2]);
+			int taille = penultiemeTerme.length();
+			
+			penultiemeTerme.delete(taille-1, taille);
+			penultiemeTerme.append(" and");
+			separator[separator.length-2]=penultiemeTerme.toString();
+			phrase.setLength(0);
+			
+			for(int i = 0; i < separator.length; i++) {
+				phrase.append(separator[i]);	
+			}
+		}
+	}
+	
+	private static boolean GestionPrenoms(String[] noms, StringBuilder phrase, StringBuilder maj) {
+	    boolean contientMaj = false;
+	    for (String nom : noms) {
+	        String SansEspace = nom.trim();
+	        if (SansEspace.toUpperCase().equals(SansEspace)) {
+	        	ajouterPrenomAvecMajuscule(maj, SansEspace);
+	            contientMaj = true; //au moins un prenom en majuscule
 	        } else {
-	            nom = nom.substring(0, 1).toUpperCase() + nom.substring(1);
-	            phrase.append(" " + nom + ",");
+	        	ajouterPrenomAvecMajuscule(phrase, SansEspace);
 	        }
 	    }
-	    
-	    phrase.deleteCharAt(phrase.length() - 1); // Supprimer la dernière virgule de la phrase d'accueil
-
-	    if (majuscule.length() > 0) {
-	        if (phrase.length() <= 7) {
-	            return "HELLO, " + majuscule.toString().trim() + " !";
-	        } else {
-	            phrase.append(". AND HELLO, ");
-	            if (length == 2) {
-	                phrase.append(" ");
-	            }
-	            phrase.append(majuscule.toString().trim() + " !");
-	        }
-	    }
-	    return phrase.toString();
+	    GestionVirgule(maj);
+	    GestionVirgule(phrase);
+	    ajoutAnd(maj);
+	    ajoutAnd(phrase);
+	    return contientMaj;
 	}
 }
-
-
-
-
-
--- test/ilu2/WelcomeTest.java
+++ test/ilu2/WelcomeTest.java
@@ -36,27 +36,32 @@ class WelcomeTest {
 	
 	@Test
 	void EX_4() {
-		assertEquals("Hello, Amy, Bob",Welcome.welcome("amy,bob"));
-		assertEquals("Hello, Amina, Mathilde",Welcome.welcome("amina,mathilde"));
-		assertEquals("Hello, Tom, Emma",Welcome.welcome("Tom,emma"));
-		assertEquals("Hello, Mathis, Nicolas",Welcome.welcome("Mathis,Nicolas"));
+		assertEquals("Hello, Amy and Bob",Welcome.welcome("amy,bob"));
+		assertEquals("Hello, Amina and Mathilde",Welcome.welcome("amina,mathilde"));
+		assertEquals("Hello, Tom and Emma",Welcome.welcome("Tom,emma"));
+		assertEquals("Hello, Mathis and Nicolas",Welcome.welcome("Mathis,Nicolas"));
 		
 	}
 	
 	@Test 
 	void EX_5() {
-		assertEquals("Hello, Amy, Bob, Jerry", Welcome.welcome("Amy,bob,jerry"));
-		assertEquals("Hello, Amina, Nicolas, Tom", Welcome.welcome("Amina,nicolas,tom"));
-		assertEquals("Hello, Mathilde, Jean, Baptiste, Emma", Welcome.welcome("mathilde,Jean,baptiste,Emma"));
+		assertEquals("Hello, Amy, Bob and Jerry", Welcome.welcome("Amy,bob,jerry"));
+		assertEquals("Hello, Amina, Nicolas and Tom", Welcome.welcome("Amina,nicolas,tom"));
+		assertEquals("Hello, Mathilde, Jean, Baptiste and Emma", Welcome.welcome("mathilde,Jean,baptiste,Emma"));
 	}
 	
 	@Test
 	void EX_6(){
-		assertEquals("Hello, Amy, Jerry. AND HELLO, BOB !", Welcome.welcome("Amy,BOB,Jerry"));
-		assertEquals("Hello, Jerry, Tom. AND HELLO, AMY, BOB !", Welcome.welcome("AMY,BOB,Jerry,tom"));
-		assertEquals("Hello, Jerry, Tom. AND HELLO, AMY, BENOIT !", Welcome.welcome("AMY,jerry,tom,BENOIT"));
-		assertEquals("HELLO, MATHILDE, NICOLAS, TOM, MATHIS !", Welcome.welcome("MATHILDE,NICOLAS,TOM,MATHIS"));
-		assertEquals("HELLO, LEO, MATHIS !", Welcome.welcome("LEO,MATHIS"));
+		assertEquals("Hello, Amy and Jerry. AND HELLO, BOB !", Welcome.welcome("Amy,BOB,Jerry"));
+		assertEquals("Hello, Jerry and Tom. AND HELLO, AMY AND BOB !", Welcome.welcome("AMY,BOB,Jerry,tom"));
+		assertEquals("Hello, Jerry and Tom. AND HELLO, AMY AND BENOIT !", Welcome.welcome("AMY,jerry,tom,BENOIT"));
 	}
 	
+	//j'ai changé le nom de test car la consigne a été changé 
+	@Test
+	void ITER_7() {
+		assertEquals("Hello, Bob, Amy and Jerry", Welcome.welcome("bob, amy, jerry"));
+		assertEquals("Hello, Bob and Jerry. AND HELLO, AMY AND JACK !", Welcome.welcome("bob, AMY, jerry, JACK"));
+		assertEquals("Hello, Tom. AND HELLO, BOB AND JERRY !", Welcome.welcome("Tom, BOB,JERRY"));
+	}
 }
